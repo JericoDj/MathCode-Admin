f348ea4624dab0c1f902dd9e4afc07222a50587d
jericodejesus <dejesusjerico528@gmail.com>
2026-01-12 07:44:50 +0800

    credits ui not finiished

--- src/utils/auth.api.tsx
+++ src/utils/auth.api.tsx
@@ -1,6 +1,6 @@
 import type { AdminUser, LoginCredentials, User, CreateUserDTO, UpdateUserDTO, UserFilterOptions } from '../types';
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 const LOCAL_STORAGE_USER_KEY = 'adminUser';
 const LOCAL_STORAGE_TOKEN_KEY = 'adminToken';
 
